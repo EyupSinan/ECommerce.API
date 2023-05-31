e48e7fceed741b4f2fbd8adfd98d9900a511ff93
esina <esina@SINAN>
2023-05-31 16:58:10 +0100

    Add project files.

---
+++ Core/ECommerceAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceAPI.Application.ViewModels.Products;
+using FluentValidation;
+
+
+namespace ECommerceAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator() {
+            RuleFor(p => p.Name).NotEmpty()
+                    .NotNull()
+                    .WithMessage("Please type the product name")
+                    .MaximumLength(150)
+                    .MinimumLength(3)
+                    .WithMessage("Please type the product name between 3 to 150 character");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Please add a value")
+                .Must(s => s >= 0)
+                .WithMessage("Stock value cannot be less than 0 ");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Please add a value")
+                .Must(s => s >= 0)
+                .WithMessage("Price value cannot be less than 0 ");
+        }
+    }
+}
---
+++ Presentation/ECommerceAPI.API/Program.cs
@@ -0,0 +1,35 @@
+using ECommerceAPI.Application.Validators.Products;
+using ECommerceAPI.Infrastructure.Filters;
+using ECommerceAPI.Persistence;
+using FluentValidation.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddPersistenceServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
